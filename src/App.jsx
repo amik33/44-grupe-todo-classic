a0620886041e2fa2844896b6579eefe9d2c825ea
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-07-24 17:11:49 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import style from './App.module.css';
 
+
 function App() {
   return (
     <div className={style.app}>
-        CONTENT 
+      <div className={style.platform}>
+          <img src='../img/cap.png' alt="logo" />
+        <div className={style.section}>
+          <h2>Shopping platform</h2>
+        </div>
+          <button className={style.button}>Get started</button>
+      </div>
+      <div className={style.future}>
+        <div className={style.skip}>
+          <a href="/">Skip</a>
+        </div>
+          <img src="../img/cap.png" alt="Future" />
+        <div className={style.section}>
+          <h3>Future</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+          <button className={style.button}>Continue</button>
+      </div>
+      <div className={style.platform}>
+          <img src="../img/cap.png" alt="Future" />
+        <div className={style.section}>
+          <h3>Future</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+          <button className={style.button}>Continue</button>
+      </div>
     </div>
   );
 }
