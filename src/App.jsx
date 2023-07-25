a2a0fd5db39d5c5b239920b02187ead1e66eb206
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-07-25 21:03:16 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Home } from './pages/Home';
 import { Future } from './pages/Future';
 import { Future1 } from './pages/Future1';
 import { Page404 } from './pages/Page404';
-import { About } from './pages/About';
 import { Services } from './pages/Services';
 import { Service } from './pages/Service';
 import { Login } from './pages/Login';
@@ -19,16 +18,18 @@ function App() {
         <Routes>
           <Route Component={Layout}>
             <Route index path="/" element={<Home />} />
-            {/* <Route path="/future" element={<Future />} />
-            <Route path="/future1" element={<Future1 />} /> */}
-            <Route path="/login" element={<Login />} />
+            {/* <Route path="/future" element={<Future />} /> */}
+            <Route path="/future1" element={<Future1 />} />
             {/* <Route path="/services" element={<Services />} /> */}
-            <Route path="/services/:serviceId" element={<Service />} />
+            {/* <Route path="/services/:serviceId" element={<Service />} />
             <Route path="/services/:serviceId/edit" element={<Service />} />
-            <Route path="/services/:serviceId/providers/:name" element={<Service />} />
+            <Route path="/services/:serviceId/providers/:name" element={<Service />} /> */}
+              <Route path="*" element={<Page404 />} />
           </Route>
           <Route Component={LayoutTwo}>
             <Route path="/future" element={<Future />} />
+            <Route path="/future1" element={<Future1 />} />
+            <Route path="/login" element={<Login />} />
             <Route path="*" element={<Page404 />} />
           </Route>
         </Routes>
--- src/pages/Future1.jsx
+++ src/pages/Future1.jsx
@@ -1,18 +1,21 @@
 import style from './Future1.module.css';
+import { Link } from 'react-router-dom';
 
 
 export function Future1 () {
     return (
-        <div className={style.future1}>
-            <div className={style.skip}>
-            <a href="/">Skip</a>
-        </div>
+    <div className={style.future1}>
+        <div className={style.header}>
+        <Link className={style.button} to='/future2'>Skip</Link>
             <img src="../img/cap.png" alt="Future" />
-        <div className={style.section}>
+        </div>
+        <div className={style.main}>
             <h3>Future</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
         </div>
+        <div className={style.footer}>
             <button className={style.button}>Continue</button>
         </div>
+    </div>
     )
 };
