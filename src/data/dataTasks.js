a207af4522052cacda9d94d33ed6001a55693c34
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-07-27 19:06:36 +0300

    User registr localStorage

---
+++ src/data/dataTasks.js
@@ -0,0 +1,18 @@
+export const tasksdata = [
+    {
+        id: 1,
+        text: 'Product',
+    },
+    {
+        id: 2,
+        text: 'Product',
+    },
+    {
+        id: 3,
+        text: 'Product',
+    },
+    {
+        id: 4,
+        text: 'Product',
+    },
+];
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,31 +1,66 @@
 import style from './Create.module.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
 export function Create () {
-    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    // const registr = `${name} ${email} ${password}`.trim();
-  
-    function updateName(event) {
-      setName(event.target.value);
+    const [errors, setErrors] = useState([]);
+    const [users, setUsers] = useState(() => JSON.parse(localStorage.getItem('users')) || []);
+
+    useEffect(() => {
+        localStorage.setItem('users', JSON.stringify(users));
+    }, [users]);
+
+    function updateUsername(event) {
+        setUsername(event.target.value);
     }
   
     function updateEmail(event) {
-      setEmail(event.target.value);
+        setEmail(event.target.value);
     }
 
     function updatePassword(event) {
         setPassword(event.target.value);
-      }
+    }
+
+    function createUser(event) {
+        const minUsernameLength = 3;
+        const maxUsernameLength = 20;
+        const minEmailLength = 6;
+        const maxEmailLength = 30;
+        const minPasswordLength = 6;
+        const maxPasswordLength = 100;
+        event.preventDefault();
+
+        const newErrors = [];
+        if (username.length < minUsernameLength || username.length > maxUsernameLength) {
+            newErrors.push('Error: username');
+        }
+
+        if (email.length < minEmailLength || email.length > maxEmailLength) {
+            newErrors.push('Error: email');
+        }
+
+        if (password.length < minPasswordLength || password.length > maxPasswordLength) {
+            newErrors.push('Error: password');
+        }
+
+        setErrors(newErrors);
+
+        if (!errors.length) {
+            setUsers((prev) => [...prev, { username, email, password }]);
+        }
+    }
+
 
     return (
         <div className={style.create}>
             <form className={style.form}>
                 <h3>Create your account</h3>
                 <div className={style.row}>
-                    <input onChange={updateName} value={name} id='name' type='text' placeholder='Name' />
+                    <input onChange={updateUsername} value={username} id='name' type='text' placeholder='Name' />
                 </div>
                 <div className={style.row}>
                     <input onChange={updateEmail} value={email} id='email' type='text' placeholder='Email' />
@@ -33,16 +68,20 @@ export function Create () {
                 <div className={style.row}>
                     <input onChange={updatePassword} value={password} id='password' type='password' placeholder='Password' />
                 </div>
+                <div className={`${style.error} ${errors.length ? style.show : ''}`}>
+                    {errors.map(err => <p>{err}</p>)}
+                </div>
                 <div className={style.read}>
                     <Link className={style.btn} to='/terms'></Link>
                     <p>Agree to our TOS (read)</p>
                 </div>
                 <div className={style.row}>
-                    <Link className={style.button} to='/login'>Register</Link>
+                    <Link onClick={createUser} className={style.button} to='/login'>Register</Link>
                     <Link className={style.button} to='/login'>Login</Link>
                 </div>
             </form>
         </div>
-  );
+
+    );
 
 }
